2369409453b4153f846e7a89ea8da1210c2e2f7d
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-09 19:12:17 +0100

    Add app blade, modify EmployeeController

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,63 +2,113 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $employees = Employee::paginate(10);
+        return view('employees.index', compact('employees'));
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('employees.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'company' => 'required|max:255',
+            'email' => 'required|email|unique:employees',
+            'phone_numbers' => 'required|array',
+            'dietary_preferences' => 'required|max:255',
+        ]);
+
+        $employee = Employee::create($validatedData);
+        
+        return redirect()->route('employees.index')->with('success', 'Employee created successfully.');
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  string  $id
+     * @return \Illuminate\Http\Response
      */
     public function show(string $id)
     {
-        //
+        $employee = Employee::findOrFail($id);
+        return view('employees.show', compact('employee'));
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  string  $id
+     * @return \Illuminate\Http\Response
      */
     public function edit(string $id)
     {
-        //
+        $employee = Employee::findOrFail($id);
+        return view('employees.edit', compact('employee'));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $id
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validatedData = $request->validate([
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'company' => 'required|max:255',
+            'email' => 'required|email|unique:employees,email,' . $id,
+            'phone_numbers' => 'required|array',
+            'dietary_preferences' => 'required|max:255',
+        ]);
+
+        $employee = Employee::findOrFail($id);
+        $employee->update($validatedData);
+
+        return redirect()->route('employees.index')->with('success', 'Employee updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  string  $id
+     * @return \Illuminate\Http\Response
      */
     public function destroy(string $id)
     {
-        //
+        $employee = Employee::findOrFail($id);
+        $employee->delete();
+
+        return redirect()->route('employees.index')->with('success', 'Employee deleted successfully.');
     }
 }
