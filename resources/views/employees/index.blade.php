614b5e430964b1c69d08661f2a9d37a1ca5a081d
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-09 22:05:18 +0100

    Implement search, filtering, and pagination in EmployeeController

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,52 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-md-12">
-        <h2>Employees</h2>
-        <a href="{{ route('employees.create') }}" class="btn btn-success mb-2">Add New Employee</a>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Phone Numbers</th>
-                    <th>Dietary Preferences</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($employees as $employee)
-                    <tr>
-                        <td>{{ $employee->id }}</td>
-                        <td>{{ $employee->first_name }}</td>
-                        <td>{{ $employee->last_name }}</td>
-                        <td>{{ $employee->company }}</td>
-                        <td>{{ $employee->email }}</td>
-                        <td>
-                            @if(is_array($employee->phone_numbers))
-                                {{ implode(', ', $employee->phone_numbers) }}
-                            @else
-                                {{ $employee->phone_numbers }}
-                            @endif
-                        </td>
-                        <td>{{ $employee->dietary_preferences }}</td>
-                        <td>
-                            <a href="{{ route('employees.show', $employee->id) }}" class="btn btn-info">View</a>
-                            <a href="{{ route('employees.edit', $employee->id) }}" class="btn btn-primary">Edit</a>
-                            <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" style="display: inline-block;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+<div class="container mt-4">
+    <h1 class="mb-4">Employee List</h1>
+
+    <form action="{{ route('employees.index') }}" method="GET">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" name="search" placeholder="Search employees" value="{{ request('search') }}">
+            <button class="btn btn-outline-secondary" type="submit">Search</button>
+        </div>
+    </form>
+
+    <table class="table table-striped table-bordered table-responsive">
+        <thead class="table-primary">
+            <tr>
+                <th><a href="{{ route('employees.index', ['sort_by' => 'first_name']) }}">First Name</a></th>
+                <th><a href="{{ route('employees.index', ['sort_by' => 'last_name']) }}">Last Name</a></th>
+                <th><a href="{{ route('employees.index', ['sort_by' => 'company']) }}">Company</a></th>
+                <th><a href="{{ route('employees.index', ['sort_by' => 'email']) }}">Email</a></th>
+                <th>Phone Numbers</th>
+                <th>Dietary Preferences</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($employees as $employee)
+            <tr>
+                <td>{{ $employee->first_name }}</td>
+                <td>{{ $employee->last_name }}</td>
+                <td>{{ $employee->company }}</td>
+                <td>{{ $employee->email }}</td>
+                <td>{{ implode(', ', $employee->phone_numbers) }}</td>
+                <td>{{ $employee->dietary_preferences }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div>
+        {{ $employees->links() }}
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Management</title>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div class="container mt-4">
         @yield('content')
     </div>
-
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
