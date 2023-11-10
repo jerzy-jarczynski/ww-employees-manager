043970af29901bc1d3a161d6daee4f5e85c91833
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-11-10 12:28:05 +0100

    Make the employee table scrollable horizontally

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -11,40 +11,43 @@
         </div>
     </form>
 
-    <table class="table table-striped table-bordered table-responsive">
-        <thead class="table-primary">
-            <tr>
-                <th><a href="{{ route('employees.index', ['sort_by' => 'first_name', 'sort_order' => request('sort_by') === 'first_name' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">First Name</a></th>
-                <th><a href="{{ route('employees.index', ['sort_by' => 'last_name', 'sort_order' => request('sort_by') === 'last_name' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">Last Name</a></th>
-                <th><a href="{{ route('employees.index', ['sort_by' => 'company', 'sort_order' => request('sort_by') === 'company' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">Company</a></th>
-                <th><a href="{{ route('employees.index', ['sort_by' => 'email', 'sort_order' => request('sort_by') === 'email' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">Email</a></th>
-                <th>Phone Numbers</th>
-                <th>Dietary Preferences</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($employees as $employee)
-            <tr>
-                <td>{{ $employee->first_name }}</td>
-                <td>{{ $employee->last_name }}</td>
-                <td>{{ $employee->company }}</td>
-                <td>{{ $employee->email }}</td>
-                <td>{{ is_array($employee->phone_numbers) ? implode(', ', $employee->phone_numbers) : $employee->phone_numbers }}</td>
-                <td>{{ $employee->dietary_preferences }}</td>
-                <td>
-                    <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}" class="btn btn-primary">Edit</a>
-                    <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteConfirmationModal" onclick="setDeleteAction('{{ route('employees.destroy', ['employee' => $employee->id]) }}')">Delete</button>
-                    </form>
-                    <a href="{{ route('employees.show', ['employee' => $employee->id]) }}" class="btn btn-success">View</a> <!-- Add this line -->
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <!-- Wrap the table in a div with horizontal scroll -->
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered">
+            <thead class="table-primary">
+                <tr>
+                    <th><a href="{{ route('employees.index', ['sort_by' => 'first_name', 'sort_order' => request('sort_by') === 'first_name' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">First Name</a></th>
+                    <th><a href="{{ route('employees.index', ['sort_by' => 'last_name', 'sort_order' => request('sort_by') === 'last_name' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">Last Name</a></th>
+                    <th><a href="{{ route('employees.index', ['sort_by' => 'company', 'sort_order' => request('sort_by') === 'company' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">Company</a></th>
+                    <th><a href="{{ route('employees.index', ['sort_by' => 'email', 'sort_order' => request('sort_by') === 'email' && request('sort_order') === 'asc' ? 'desc' : 'asc']) }}">Email</a></th>
+                    <th>Phone Numbers</th>
+                    <th>Dietary Preferences</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($employees as $employee)
+                <tr>
+                    <td>{{ $employee->first_name }}</td>
+                    <td>{{ $employee->last_name }}</td>
+                    <td>{{ $employee->company }}</td>
+                    <td>{{ $employee->email }}</td>
+                    <td>{{ is_array($employee->phone_numbers) ? implode(', ', $employee->phone_numbers) : $employee->phone_numbers }}</td>
+                    <td>{{ $employee->dietary_preferences }}</td>
+                    <td>
+                        <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}" class="btn btn-primary">Edit</a>
+                        <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteConfirmationModal" onclick="setDeleteAction('{{ route('employees.destroy', ['employee' => $employee->id]) }}')">Delete</button>
+                        </form>
+                        <a href="{{ route('employees.show', ['employee' => $employee->id]) }}" class="btn btn-success">View</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
     <div>
         {{ $employees->appends(['sort_by' => request('sort_by'), 'sort_order' => request('sort_order')])->links('vendor.pagination.bootstrap-5') }}
@@ -71,7 +74,7 @@
                 </div>
             </div>
         </div>
-    </div>    
+    </div>
 </div>
 
 <script>
